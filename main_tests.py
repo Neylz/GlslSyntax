e8e230c30f90b036832c052909686b8b4cf2d583
Neylz <neylzvii@gmail.com>
2024-02-15 11:15:22 +0100

    vec3 tests

--- main_tests.py
+++ main_tests.py
@@ -16,16 +16,48 @@ print(f"v1.x = {v1.x}\n"
       f"v1.xy = {v1.xy}\n"
       f"v1.yx = {v1.yx}\n")
 
-print("v1 + v2 =", v1 + v2)
-print("v1 - v2 =", v1 - v2)
-print("v1 * v2 =", v1 * v2)
-print("v1 * 2 =", v1 * 2)
-print("2 * v1 =", 2 * v1)
-print("v1.length() =", v1.length())
-print("v1.normalize() =", v1.normalize())
-print("v1.dot(v2) =", v1.dot(v2))
+print(f"v1 + v2 = {v1 + v2}\n"
+      f"v1 - v2 = {v1 - v2}\n"
+      f"v1 * v2 = {v1 * v2}\n"
+      f"v1 * 2 = {v1 * 2}\n"
+      f"2 * v1 = {2 * v1}\n"
+      f"v1.length() = {v1.length()}\n"
+      f"v1.normalize() = {v1.normalize()}\n"
+      f"v1.dot(v2) = {v1.dot(v2)}\n")
 
 print(f"v1 == v2 = {v1 == v2}\n"
       f"v1 != v2 = {v1 != v2}\n"
       f"v1 == vec2(1, 2) = {v1 == vec2(1, 2)}\n")
+
+
+print("\n> Testing vec3() operations")
+v3 = vec3(1, 2, 3)
+v4 = vec3(4, 5, 6)
+print(f"v3 = {v3} and v2 = {v2}\n")
+
+print(f"v3.size = {v3.size}\n")
+
+print(f"v3.x = {v3.x}\n"
+      f"v3.y = {v3.y}\n"
+      f"v3.z = {v3.z}\n"
+      f"v3.xy = {v3.xy}\n"
+      f"v3.yz = {v3.yz}\n"
+      f"v3.zx = {v3.zx}\n"
+      f"v3.xyz = {v3.xyz}\n"
+      f"v3.yxz = {v3.yxz}\n"
+      f"v3.zxy = {v3.zxy}\n")
+
+print(f"v3 + v4 = {v3 + v4}\n"
+      f"v3 - v4 = {v3 - v4}\n"
+      f"v3 * v4 = {v3 * v4}\n"
+      f"v3 * 2 = {v3 * 2}\n"
+      f"2 * v3 = {2 * v3}\n"
+      f"v3.length() = {v3.length()}\n"
+      f"v3.normalize() = {v3.normalize()}\n"
+      f"v3.dot(v4) = {v3.dot(v4)}\n")
+
+print(f"v3 == v4 = {v3 == v4}\n"
+      f"v3 != v4 = {v3 != v4}\n"
+      f"v3 == vec3(1, 2, 3) = {v3 == vec3(1, 2, 3)}\n")
+
 
